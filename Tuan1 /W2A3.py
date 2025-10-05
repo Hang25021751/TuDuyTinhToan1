9921cec830a29e173c68e3da62f594a9ef6d25f5
Hang25021751 <kieuanh1803470@gmail.com>
2025-10-06 00:02:49 +0700

    Create W2A3.py

---
+++ Tuan1 /W2A3.py
@@ -0,0 +1,14 @@
+a = int(input("nhap vao a:"))
+b = int(input("nhap vao b:"))
+tong = a + b 
+hieu = a - b 
+tich = a * b 
+chia_thuc = round(a/b , 2 )
+phan_nguyen = a//b 
+phan du = a%b 
+print("tong:", tong )
+print("hieu:", hieu)
+print("tich:", tich)
+print("chia_thuc:", chia_thuc)
+print("phan_nguyen",phan_nguyen)
+print("phan_du",phan_du)
