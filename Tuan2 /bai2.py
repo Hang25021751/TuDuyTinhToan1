4e94df7978586b7862b4de987669a2766daf4519
Hang25021751 <kieuanh1803470@gmail.com>
2025-10-08 19:38:37 +0700

    Delete Tuan2 /bai2.py

--- Tuan2 /bai2.py
+++
@@ -1,9 +0,0 @@
-a = int(input("nhap vao chieu rong:"))
-b = int(input("nhap vao chieu dai:")) 
-# diện tích hình chữ nhật 
-s1 = a*b 
-# diện tích hình tròn 
-s2 = 3.14 * (a/2)**2
-Dien_tich_trong_cay= s1-s2 
-
-print(round(Dien_tich_trong_cay , 2))
